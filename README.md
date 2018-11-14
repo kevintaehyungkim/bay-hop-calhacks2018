0b850d15bfbbbd508883893c124546ec2502f679
kevin3238 <kevin3238@berkeley.edu>
2018-11-13 19:07:55 -0800

    readme update

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# bay-hop-calhacks2018
-Optimize the use of public transit and ride share to minimize travel time in the SF Bay Area (Cal Hacks 2018)
+# Bay Hop
+Intermodal routing application to optimize travel time in the SF Bay Area through the combination of all modes of tranportation (Cal Hacks 2018)
+
+<img src="img/home.png" width="500">
+<img src="img/results.png" width="500">
+
+## Acknowledgments
+
+* Google Maps API
+* Uber API
+* Lyft API
+* Here API
+* BART API
--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def route():
 		distance = get_distance(geocode_start_dest)
 
 		results = {'start': start, 'end': dest, 'startLat': geocode_start[0], 'startLng': geocode_start[1], 'endLat': geocode_dest[0], 'endLng': geocode_dest[1]}
-		route_info = generate_graph([results['startLat'],results['startLng']], [results['endLat'],results['endLng']], [1,0,0,0,1])
+		route_info = generate_graph([results['startLat'],results['startLng']], [results['endLat'],results['endLng']], [0,0,0,0,1])
 
 		# [[0, 59], [['S', 'Powell St.', 'W'], ['Powell St.', '12th St. Oakland City Center', 'B'], ['12th St. Oakland City Center', 'Downtown Berkeley', 'B'], ['Downtown Berkeley', 'E', 'W']]]
 
